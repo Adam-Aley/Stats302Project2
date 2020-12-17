7b8958ae073c4d229abc35bbe1008ba328bcead3
Adam Aley <aleyad@uw.edu>
2020-12-17 01:03:33 -0800

    Update

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -9,7 +9,9 @@ test_that("my_knn_cv works properly", {
       1, 5), "list")
 })
 
-test_that("incorrect input throws error", {
-  expect_error(my_knn_cv(new_penguins, "wrong", 1, 5))
-  expect_error(my_knn_cv(new_penguins, "species", "one", 5))
+test_that("wrong syntax throw errors", {
+  expect_error(my_knn_cv(new_penguins, "wrong",  1, 5))
+  expect_error(my_knn_cv(new_penguins, "species", "number",  5))
+  expect_error(my_knn_cv(new_penguins, "species",  1))
+  expect_error(my_knn_cv(gapminder, "species", 1,  5))
 })
--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -8,3 +8,7 @@ test_that("non formula throws error" , {
     expect_error(my_lm(new_penguins$species, new_penguins))
     })
 
+test_that("bad syntax throws errors", {
+  expect_error(my_lm("a", bill_length_mm ~ bill_depth_mm + flipper_length_mm))
+  expect_error(my_lm(new_penguins, 2))
+})
